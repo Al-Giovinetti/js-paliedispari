8a3de5e64b27c32e9cc85ef4e3079c3fb01c0f15
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-22 15:53:35 +0200

    Funzione commentata

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
-//Chiedere all’utente di inserire una parola
+//Chiedere all’utente di inserire una parola per sapere se è un palindromo oppure no
 
 const userInput = prompt("inserisci una parola")
 
@@ -9,16 +9,20 @@ if(isThisPalindrome(userInput)){
 }
 
 
-
-function isThisPalindrome(wordFunction){
+/**Lets you know if a word is a palindrim
+ * 
+ * @param wordA word entered by user
+ * @returns if it's a palindrome it's true else it's folse
+ */
+function isThisPalindrome(wordA){
     let reverse = ""
 
-    for( let i=wordFunction.length; i>=0; i--){
+    for( let i=wordA.length; i>=0; i--){
         reverse = reverse + wordFunction.charAt(i)
         console.log(reverse)   
     }
 
-    if (wordFunction == reverse){
+    if (wordA == reverse){
         return true
     }else{
         return false
