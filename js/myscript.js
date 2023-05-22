ff0ac5e0e63bc249ffb8b776b991814041a921a6
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-22 15:37:13 +0200

    Correzione del for per palindromo

--- js/myscript.js
+++ js/myscript.js
@@ -2,13 +2,20 @@
 
 const userInput = prompt("inserisci una parola")
 
+if(isThisPalindrome(userInput)){
+    console.log("E' un palindromo")
+}else{
+    console.log("non è un palindromo")
+}
+
 
 
 function isThisPalindrome(wordFunction){
+    let reverse = ""
 
-    for( let i=wordFunction.length; i=0; i--){
-        let reverse = ""
-        reverse = reverse + wordFunction.charAt(i)   
+    for( let i=wordFunction.length; i>=0; i--){
+        reverse = reverse + wordFunction.charAt(i)
+        console.log(reverse)   
     }
 
     if (wordFunction == reverse){
