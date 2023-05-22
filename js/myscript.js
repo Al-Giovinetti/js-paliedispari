255607a55b96db3370c2a75af2aacf6295ca7df8
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-22 15:23:21 +0200

    Impostazione funzione palindromo

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+//Chiedere all’utente di inserire una parola
+
+const userInput = prompt("inserisci una parola")
+
+function isThisPalindrome(wordFunction){
+
+    for( let i=wordFunction.length; i=0; i--){
+        const reverse = ""
+        reverse = reverse +   
+    }
+
+    if (wordFunction == reverse){
+        return true
+    }else{
+        return false
+    }
+}
